7893e673e91749c075057b412fe21de4dac38f2e
Anselmo Parente Martins <anselmoparente@gmail.com>
2022-04-09 12:14:58 -0300

    Created First Window

--- src/main.py
+++ src/main.py
@@ -1,3 +1,24 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+class Window (QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.top = 100
+        self.left = 100
+        self.width = 800
+        self.height = 600
+        self.title = "First Window"
+        self.ChargeWindow()
+    
+    def ChargeWindow(self):
+        self.setGeometry(self.left, self.top, self.width, self.height), 
+        self.setWindowTitle(self.title),
+        self.show()
+
 
 if __name__ == "__main__":
-    print('Só pra testar um ngc')
+    app = QApplication(sys.argv)
+    w = Window()
+    sys.exit(app.exec_())
